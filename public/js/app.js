b0794613a42446f39ba982e145c4a8d3f52552ae
Imesha Sudasingha <imesha@highflyer.lk>
2016-05-11 16:19:57 +0530

    Prescribe medicine page -> add drugs part completed

--- public/js/app.js
+++ public/js/app.js
@@ -1,48 +1,131 @@
-var app = angular.module('HIS', [], function ($interpolateProvider) {
+var app;
+
+app = angular.module('HIS', [], function ($interpolateProvider) {
         $interpolateProvider.startSymbol('[[');
         $interpolateProvider.endSymbol(']]');
     })
-    .controller('PrescriptionController', ['$scope', '$http', 'api', function ($scope, $http, api) {
-        $scope.drugs = [];
-        $scope.dosages = [];
-        $scope.frequencies = [];
-        $scope.periods = [];
+    .controller('PrescriptionController', ['$scope', '$http', 'api', '$filter', '$timeout',
+        function ($scope, $http, api, $filter, $timeout) {
+            $scope.drugs = [];
+            $scope.dosages = [];
+            $scope.frequencies = [];
+            $scope.periods = [];
 
-        $scope.drug = null;
-        $scope.period = null;
-        $scope.frequency = null;
-        $scope.dosage = null;
+            //things to be submitted
+            $scope.prescribedDrugs = [];
+            $scope.complaints="";
+            $scope.investigations="";
+            $scope.diagnosis="";
+            $scope.remarks="";
 
-        $scope.baseUrl = "";
-        $scope.token = "";
+            $scope.drug = null;
+            $scope.period = null;
+            $scope.frequency = null;
+            $scope.dosage = null;
 
-        $scope.init = function () {
-            api.getDrugs($scope.baseUrl, $scope.token).then(function (drugs) {
-                $scope.drugs = drugs;
-            });
+            $scope.baseUrl = "";
+            $scope.token = "";
 
-            api.getDosages($scope.baseUrl, $scope.token).then(function (data) {
-                if (data != null) {
-                    $scope.dosages = data.dosages;
-                    $scope.frequencies = data.frequencies;
-                    $scope.periods = data.periods;
-                    console.log($scope.dosages);
-                    console.log($scope.frequencies);
-                    console.log($scope.periods);
+            //for error handling
+            $scope.hasDrugError = false;
+            $scope.error = "";
+
+
+            //to measure the timeout of errors
+            $scope.drugErrorTimeout=null;
+
+
+            /**
+             * Init function. loads relevant data from the API
+             */
+            $scope.init = function () {
+                api.getDrugs($scope.baseUrl, $scope.token).then(function (drugs) {
+                    $scope.drugs = drugs;
+                });
+
+                api.getDosages($scope.baseUrl, $scope.token).then(function (data) {
+                    if (data != null) {
+                        $scope.dosages = data.dosages;
+                        $scope.frequencies = data.frequencies;
+                        $scope.periods = data.periods;
+                    }
+                });
+            };
+
+
+            /**
+             * Adds a drug to the prescribed drugs list. At least a drug and an dosage has to be selected.
+             */
+            $scope.add = function () {
+                var d, dose, frequency, period;
+
+                //initially check whether a drug/dosage is selected
+                if (!$scope.drug || !$scope.dosage) {
+                    $scope.showDrugError("You must select a drug and a dosage to add to the prescription");
+                    return;
                 }
-            });
-        }
 
-        $scope.add = function () {
-            console.log($scope.drug+" "+$scope.dosage+" "+$scope.frequency+" "+$scope.period);
-        }
+                //search for the selected drug, frequency, dosage and period.
+                if ($scope.drug) {
+                    d = $filter('filter')($scope.drugs, {id: $scope.drug}, false);
+                }
+                if ($scope.dosage) {
+                    dose = $filter('filter')($scope.dosages, {id: $scope.dosage}, false);
+                }
+                if ($scope.frequency) {
+                    frequency = $filter('filter')($scope.frequencies, {id: $scope.frequency}, false);
+                }
+                if ($scope.period) {
+                    period = $filter('filter')($scope.periods, {id: $scope.period}, false);
+                }
 
-    }])
+                //check if the selected drug is already added to the list.
+                var arr = $filter('filter')($scope.prescribedDrugs, {drug: {id: d[0].id}}, false);
+                if (arr.length > 0) {
+                    $scope.showDrugError("Drug already added to the prescription");
+                    return;
+                }
+
+                $scope.prescribedDrugs.push(
+                    {
+                        drug: d[0],
+                        dose: dose[0],
+                        frequency: frequency && frequency.length > 0 ? frequency[0] : null,
+                        period: period && period.length > 0 ? period[0] : null,
+                        type: 1
+                    }
+                );
+            };
+
+
+            /**
+             * Helper method to show an error. An error will be visible for 5 seconds
+             * @param message
+             */
+            $scope.showDrugError = function (message) {
+                $scope.error = message;
+                $scope.hasDrugError = true;
+                $timeout.cancel($scope.drugErrorTimeout);
+                $scope.drugErrorTimeout = $timeout(function () {
+                    $scope.hasDrugError = false;
+                }, 5000);
+            };
+
+
+            /**
+             * Remove a prescribed drug from the list
+             * @param index
+             */
+            $scope.removeDrug = function (index) {
+                $scope.prescribedDrugs.splice(index, 1);
+            }
+
+        }])
     .service('api', ['$http', function ($http) {
         return {
             /**
              * Get the drugs from the server
-             * @param url
+             * @param baseUrl
              * @param token
              * @returns {*}
              */
--- resources/views/patients/tabs/prescribeMedicine.blade.php
+++ resources/views/patients/tabs/prescribeMedicine.blade.php
@@ -10,28 +10,32 @@
         <div class="form-group">
             <label class="col-md-3 col-sm-12 control-label">Presenting Complaints</label>
             <div class="col-md-9 col-sm-12">
-                <textarea id="presentingComplaints" placeholder="Presenting Complaints" class="form-control"></textarea>
+                <textarea id="presentingComplaints" placeholder="Presenting Complaints" ng-model="complaints"
+                          class="form-control"></textarea>
             </div>
         </div>
 
         <div class="form-group">
             <label class="col-md-3 col-sm-12 control-label">Investigations</label>
             <div class="col-md-9 col-sm-12">
-                <textarea id="prescriptionInvestigations" placeholder="Investigations" class="form-control"></textarea>
+                <textarea id="prescriptionInvestigations" placeholder="Investigations" ng-model="investigations"
+                          class="form-control"></textarea>
             </div>
         </div>
 
         <div class="form-group">
             <label class="col-md-3 col-sm-12 control-label">Diagnosis</label>
             <div class="col-md-9 col-sm-12">
-                <input id="prescriptionDiagnosis" placeholder="" class="form-control" type="text">
+                <input id="prescriptionDiagnosis" placeholder="" ng-model="diagnosis" class="form-control"
+                       type="text">
             </div>
         </div>
 
         <div class="form-group">
             <label class="col-md-3 col-sm-12 control-label">Other Remarks</label>
             <div class="col-md-9 col-sm-12">
-                <textarea id="prescriptionRemarks" placeholder="Remarks" class="form-control"></textarea>
+                <textarea id="prescriptionRemarks" ng-model="remarks" placeholder="Remarks"
+                          class="form-control"></textarea>
             </div>
         </div>
 
@@ -40,7 +44,14 @@
             <div class="box-header">
                 <h4 class="box-title">Drugs</h4>
             </div>
+
             <div class="box-body">
+
+                <div class="alert alert-danger alert-dismissable" ng-show="hasDrugError" ng-cloak>
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                    <h4><i class="icon fa fa-ban"></i> Oops!</h4>
+                    [[error]]
+                </div>
 
                 <div class="col-md-12">
                     <div class="col-md-8 col-md-offset-2">
@@ -48,7 +59,7 @@
                         <div class="col-sm-12">
                             <select id="prescriptionDrug" class="form-control" size="6" ng-model="drug">
                                 <option ng-repeat="drug in drugs" value="[[drug.id]]">
-                                    [[drug.name]] [[drug.quantity]]
+                                    [[drug.name]] [[drug.quantity | number:0]]
                                 </option>
                             </select>
                         </div>
@@ -71,9 +82,9 @@
                         <label class="col-sm-12 text-center">Frequency (Optional)</label>
                         <div class="col-sm-12">
                             <select id="prescriptionFrequency" class="form-control" size="6" ng-model="frequency">
-                                <option ng-repeat="frequency in frequencies track by frequency.id"
-                                        value="[[frequency.id]]">
-                                    [[frequency.description]]
+                                <option ng-repeat="f in frequencies track by f.id"
+                                        value="[[f.id]]">
+                                    [[f.description]]
                                 </option>
                             </select>
                         </div>
@@ -83,16 +94,21 @@
                         <label class="col-sm-12 text-center">Period (Optional)</label>
                         <div class="col-sm-12">
                             <select id="prescriptionPeriod" class="form-control" size="6" ng-model="period">
-                                <option ng-repeat="period in periods track by period.id" value="[[period.id]]">
-                                    [[period.description]]
+                                <option ng-repeat="p in periods track by p.id" value="[[p.id]]">
+                                    [[p.description]]
                                 </option>
                             </select>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <div class="box-footer">
+                <button class="btn btn-primary pull-right" ng-click="add()">
+                    Add
+                </button>
+            </div>
         </div>
-        <button class="btn btn-danger" ng-click="add()"></button>
         {{-- /Area to add drugs --}}
 
         {{-- Area to show drugs --}}
@@ -101,7 +117,7 @@
                 <h4 class="box-title">Prescription</h4>
             </div>
             <div class="box-body">
-                <table class="table table-condensed table-hover">
+                <table class="table table-condensed table-bordered table-hover text-center">
                     <thead>
                     <tr>
                         <th>Drug Name</th>
@@ -110,10 +126,16 @@
                     </tr>
                     </thead>
                     <tbody>
-                    <tr ng-repeat="drug in prescribedDrugs">
-                        <td>[[drug.name]]</td>
-                        <td>[[drug.dose]]</td>
-                        <td></td>
+                    <tr ng-repeat="d in prescribedDrugs track by $index">
+                        <td>[[d.drug.name]]</td>
+                        <td>
+                            [[d.dose.description]] [[d.frequency.description]] [[d.period.description]]
+                        </td>
+                        <td>
+                            <button class="btn btn-sm btn-danger" ng-click="removeDrug([[$index]])">
+                                <i class="fa fa-recycle"></i>
+                            </button>
+                        </td>
                     </tr>
                     </tbody>
                 </table>
