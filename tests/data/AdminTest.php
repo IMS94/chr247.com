7a2447bb10e3366afc2a413e51bc7467c8eac60f
Imesha Sudasingha <imesha@highflyer.lk>
2016-06-16 22:01:22 +0530

    Tests for models including, Admin, Queue written

--- tests/data/AdminTest.php
+++ tests/data/AdminTest.php
@@ -1,11 +1,27 @@
 <?php
 namespace Tests\Data;
 
+use App\Admin;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
 class AdminTest extends TestCase {
+
+    use DatabaseTransactions;
+
+    public function testCreateAndDeleteAdmin() {
+        $admin = new Admin();
+        $admin->username = "TestAdmin";
+        $admin->password = bcrypt("Password");
+        $admin->save();
+
+        $this->seeInDatabase('admins', ['username' => $admin->username]);
+
+        $admin->delete();
+
+        $this->dontSeeInDatabase('admins', ['username' => $admin->username]);
+    }
 
 }
