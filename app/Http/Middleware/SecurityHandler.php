d16d99d10148e42a68faeb84613bf5c6a7706f99
Imesha <imesha.sudasinghe@gmail.com>
2016-09-11 19:11:54 +0530

    Renamed session coockie. Added security headers to the responses.

---
+++ app/Http/Middleware/SecurityHandler.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class SecurityHandler {
+
+    /**
+     * Adds essential security headers to the HTTP responses. To prevent XSS attacks and so on
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next) {
+        $response = $next($request);
+        $response->header("X-XSS-Protection", "1; mode=block");
+        $response->header("X-Frame-Options", "SAMEORIGIN");
+        $response->header("X-Content-Type-Options", "nosniff");
+        $response->header("Strict-Transport-Security", "max-age=31536000");
+        $response->header("Content-Security-Policy", "*");
+
+        return $response;
+    }
+}
