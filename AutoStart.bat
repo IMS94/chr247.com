deec81941b104f7e6847d4633ef1fc3f2341ae13
Guchie <francisguchie@gmail.com>
2019-07-24 10:06:37 +0000

    Create AutoStart.bat

---
+++ AutoStart.bat
@@ -0,0 +1,14 @@
+@echo off
+rem this is for windows users only
+rem change the drive path to what suits your server 
+re
+
+
+cd C:\path to folder\
+
+start php artisan serve --host xx.xx.xx.xx --port 8000
+
+rem auto display of the web browser 
+rem this will automatically open the application using your default set web browser
+
+start http://xx.xx.xx.xx:8000
